4792d40f2350a9df00b9aa92a99baa5f1032c33e
Anika S <ankagugu@gmail.com>
2019-06-28 18:05:55 +0000

    still trying animations

--- bootstrap-4.1.2-dist/js/portfoliojs.js
+++ bootstrap-4.1.2-dist/js/portfoliojs.js
@@ -2,13 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.getElementById("index_fade").style.opacity = "0";
   } else {
     document.getElementById("index_fade").style.opacity = "1";
   }
   
-  if (document.body.scrollTop > 330 || document.documentElement.scrollTop > 330) {
+  if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
     document.getElementById("index_stay").style.opacity = "0";
   } else {
     document.getElementById("index_stay").style.opacity = "1";
