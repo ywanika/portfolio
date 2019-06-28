4104c7194145812aa4c49d5aff8b95eade882fba
Anika S <ankagugu@gmail.com>
2019-06-28 19:23:50 +0000

    need order to blocks in jumbotron

--- bootstrap-4.1.2-dist/js/portfoliojs.js
+++ bootstrap-4.1.2-dist/js/portfoliojs.js
@@ -1,17 +1,17 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+    
+  document.getElementById("index_stay").style.opacity = 1 - (document.documentElement.scrollTop - 150) / 170;
   
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     document.getElementById("index_fade").style.opacity = "0";
   } else {
     document.getElementById("index_fade").style.opacity = "1";
   }
-  
-  if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
-    document.getElementById("index_stay").style.opacity = "0";
-  } else {
-    document.getElementById("index_stay").style.opacity = "1";
-  }
-  
+
 }
+
+/*window.scroll=function(){
+    ("#index_stay").css("opacity", 1 - window.scrollTop() / 280);
+  };*/
