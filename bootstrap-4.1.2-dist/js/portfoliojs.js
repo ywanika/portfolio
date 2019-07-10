92cf0dabf2e1e425c8e447215a58f4e5e980e5a9
A. Sharma <ankagugu@gmail.com>
2019-07-10 16:16:22 -0700

    need to be scalable

--- bootstrap-4.1.2-dist/js/portfoliojs.js
+++ bootstrap-4.1.2-dist/js/portfoliojs.js
@@ -1,14 +1,15 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    
-  document.getElementById("index_stay").style.opacity = 1 - (document.documentElement.scrollTop - 150) / 170;
-  
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("index_fade").style.opacity = "0";
-  } else {
-    document.getElementById("index_fade").style.opacity = "1";
-  }
+    if (document.documentElement.scrollTop != 0){
+        var x = document.documentElement.scrollTop
+    } else{
+        var x = document.body.scrollTop
+    }
+
+    document.getElementById("index_stay").style.opacity = 1 - (x - 150) / 170;
+
+    document.getElementById("index_fade").style.opacity = 1 - (x-20) / 50;
 
 }
 
